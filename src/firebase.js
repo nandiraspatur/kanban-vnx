e5671edcbc62b9b1b888c0dba796d47a7bbc8819
Nandira Paturohman <nandirasp@gmail.com>
2017-12-06 14:02:35 +0700

    change database url

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 import { initializeApp } from 'firebase'
 
 const app = initializeApp({
-  authDomain: 'https://vnx-project.firebaseapp.com',
-  databaseURL: 'https://vnx-project.firebaseio.com'
+  authDomain: 'https://kanban-vnx.firebaseapp.com',
+  databaseURL: 'https://kanban-vnx.firebaseio.com'
 })
 
 export const db = app.database()
